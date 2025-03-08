fbd63291d53f854d4e2bc6061392d2f2fcb02c99
Player531 <96776640+Player531@users.noreply.github.com>
2025-03-08 10:44:38 +0530

    Update config.py

--- Assignment/Todo_REST_API/config.py
+++ Assignment/Todo_REST_API/config.py
@@ -3,5 +3,4 @@ class Config(object):
     SECRET_KEY = '7d9a849a65fb3f95ca2f61b71848542b5cf52135599fd591392dc72b076ba2b5'
     file_path = os.path.abspath(os.getcwd())+"\db.sqlite"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + file_path
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
